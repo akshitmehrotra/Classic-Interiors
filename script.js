993f962757ef5f27c0862c131012b408f0142634
Akshit <akshitmehrotra0@gmail.com>
2023-02-04 03:19:03 +0530

    First Commit
    Basic First Page

---
+++ script.js
@@ -0,0 +1,33 @@
+(function () {
+
+  var navButton = document.querySelector("#nav-menu-button");
+  var navUl = document.querySelector(".nav-ul");
+
+  function toggleMobileMenu() {
+    navUl.classList.toggle("hide-ul");
+  }
+
+  navButton.onclick = toggleMobileMenu;
+}());
+
+const counters = document.querySelectorAll('.value');
+const speed = 400;
+
+counters.forEach( counter => {
+   const animate = () => {
+      const value = +counter.getAttribute('akhi');
+      const data = +counter.innerText;
+     
+      const time = value / speed;
+     if(data < value) {
+          counter.innerText = Math.ceil(data + time);
+          setTimeout(animate, 1);
+        }else{
+          counter.innerText = value;
+        }
+     
+   }
+   animate();
+  });
+   
+
